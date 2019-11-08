312c9e8ba0df9b89576d58929f8cd50d08886b4b
Kristin <57033702+papercranecode@users.noreply.github.com>
2019-11-08 16:37:35 +0000

    Delete name.php

--- name.php
+++
@@ -1,8 +0,0 @@
-<?php
-$firstName = "Kristin";
-$lastName = "Eriksen";
-
-echo "My name is " . $firstName . " " . $lastName . " and I live in Bristol.";
-echo "<br /> $firstName $lastName thinks life is pretty good :)";
-
-?>
