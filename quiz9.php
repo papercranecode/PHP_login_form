c874b6705eb3dca88f193b03bb8f250bf5a6b109
Kristin <57033702+papercranecode@users.noreply.github.com>
2019-11-08 16:43:08 +0000

    Delete quiz9.php

--- quiz9.php
+++
@@ -1,6 +0,0 @@
-For each describe "where" I am, and what commands I ran to get there:
-
-Bill-MacBook:home mac$ -> you are in a folder called home. you have run cd filepath to home
-vagrant@scotchbox:~$ -> you are in the scotchbox secure ssh tunnel. you have run vagrant ssh.
-root@scotchbox:/var$ -> you are in the var folder of the scotchbox root.
-mysql > -> you are in MySQL. you have run mysql -u root -proot
