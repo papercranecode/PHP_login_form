fac67ab8f45b68f8300f919f23dc2e369f00a3da
papercranecode <papercranecode@users.noreply.github.com>
2019-11-08 14:50:44 +0000

    working on quiz

---
+++ quiz10.php
@@ -0,0 +1,29 @@
+<?php
+$class['paragraph'] = 'content';
+$intro = 'hello';
+$line1 = 'This is line 1';
+$line2 = 'This is line 2';
+
+echo "<p>$intro" . "<br>" .
+
+$line1 . "</p>" .
+
+"<p class = $class['paragraph']>" $line2 ". </p>"; 
+?>
+
+<script type="text/javascript">
+jQuery(document).ready(function(){
+    var line1 = [<?php
+    $first = true;
+    for($i=1;$i < 10;$i++){
+        if ($first){
+            $first = false;
+        }else{
+            echo ', ';
+        }
+        echo '[';
+        echo "'".date("Y-m-d", strtotime('2016-10-".$i))". 1:00AM"';
+        echo ']';
+    } ?>];
+});
+</script>
--- quiztest.php
+++ quiztest.php
@@ -1,7 +1,14 @@
+
 <?php
-$email = 'oliward@gmail.com';
-$hashed_and_salted_password = 'i3289';
-$salt = 'k3i2o';
-$activation_code = "kjk39";
-$query = "INSERT INTO `users` ('email', 'password', 'salt', 'activation_code') VALUES ("$email", '.$hashed_and_salted_password.'", "'.$salt.'', `$activation_code`);";
+$class['paragraph'] = 'content';
+$intro = 'hello';
+$line1 = 'This is line 1';
+$line2 = 'This is line 2';
+
+echo "<p>$intro" . "<br>" .
+
+$line1 . "</p>" .
+
+"<p class = $class['paragraph']>" $line2 ". </p>"; 
+
 ?>
